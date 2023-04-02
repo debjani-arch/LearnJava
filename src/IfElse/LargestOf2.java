817d7b0a27ad927018a1523ce470db8056700a30
debjani-arch <debjanide2017@gmail.com>
2023-04-02 21:02:13 +0530

    added code for finding largest number amongst two.

---
+++ src/IfElse/LargestOf2.java
@@ -0,0 +1,18 @@
+package IfElse;
+
+import java.util.Scanner;
+
+public class LargestOf2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        if (a > b) {
+            System.out.println(a + " is largest");
+        } else if (b > a) {
+            System.out.println(b + " is largest");
+        } else {
+            System.out.println("both are equal");
+        }
+    }
+}

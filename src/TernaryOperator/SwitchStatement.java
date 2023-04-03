81f37fbe8c921c1eca55565282f315bef8dcf0ee
debjani-arch <debjanide2017@gmail.com>
2023-04-03 16:53:58 +0530

    used switch statement to check test case

---
+++ src/TernaryOperator/SwitchStatement.java
@@ -0,0 +1,19 @@
+package TernaryOperator;
+
+public class SwitchStatement {
+    public static void main(String[] args) {
+        int number = 10;
+        switch(number) {
+            case 1: System.out.println("chocolate");
+                    break;
+            case 2: System.out.println("choco-chips");
+                    break;
+            case 3: System.out.println("milk-chocolate");
+                    break;
+            default:System.out.println("Never Give up");
+
+
+        }
+
+    }
+}

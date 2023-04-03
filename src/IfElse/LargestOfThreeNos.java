5773d12220d8c418948a30b824da9e8f0b870f53
debjani-arch <debjanide2017@gmail.com>
2023-04-03 15:26:36 +0530

    Class for finding largest of three

---
+++ src/IfElse/LargestOfThreeNos.java
@@ -0,0 +1,18 @@
+package IfElse;
+import java.util.*;
+public class LargestOfThreeNos {
+    public static void main(String[] args) {
+        int A = 1, B = 3, C = 6;
+
+        if (( A > B) && ( A > C)){
+            System.out.println(A +" is largest");
+        }
+        else if (( B > A) && ( B > C)){
+            System.out.println(B +" is largest");
+        }
+        else {
+            System.out.println(C +" is largest");
+        }
+    }
+
+}

12fec667a68e5c786b49df1aff014d3239151f6c
debjani-arch <debjanide2017@gmail.com>
2023-04-03 15:52:12 +0530

    checked whether passed or failed

---
+++ src/TernaryOperator/CheckIfPassOrFail.java
@@ -0,0 +1,9 @@
+package TernaryOperator;
+
+public class CheckIfPassOrFail {
+    public static void main(String[] args) {
+        int marks = 32;
+        String results = (marks >= 33) ? "Pass" : "Fail";
+        System.out.println(results);
+    }
+}

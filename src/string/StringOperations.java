1feb43b6fc2db0a6d8e70d276cae79afd7858fd3
debjani-arch <debjanide2017@gmail.com>
2023-04-12 19:49:18 +0530

    added remaining ops

--- src/string/StringOperations.java
+++ src/string/StringOperations.java
@@ -67,9 +67,20 @@ public class StringOperations {
         System.out.println(a.endsWith("ell"));
         System.out.println(a.endsWith("lle"));
 
-        //
+        //concat
+        a = "Debjani";
+        b = "De";
+        System.out.println(a+" "+b);
 
+        //toUpperCase and toLowerCase
+        System.out.println(a.toLowerCase());
+        System.out.println(a.toUpperCase());
 
+        //toCharArray
+        char[] charArray = a.toCharArray();
+        for(int i =0; i<charArray.length; i++) {
+            System.out.println(charArray[i]);
+        }
 
     }
 }

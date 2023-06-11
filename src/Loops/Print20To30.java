17bfc70feb7847b86d193a8551abae970a77b209
debjani-arch <debjanide2017@gmail.com>
2023-06-11 13:58:02 +0530

    Print numbers 20 to 30 (using while loop)

---
+++ src/Loops/Print20To30.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class Print20To30 {
+    public static void main(String[] args) {
+        int i=20;
+        while (i<=30) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}

71de6e09bd2930ca2fb47a249679f45634db3319
debjani-arch <debjanide2017@gmail.com>
2023-06-11 14:20:51 +0530

    Sum of first N natural Numbers(using while loop)

---
+++ src/Loops/SumOfFirstNNaturalNumbers.java
@@ -0,0 +1,18 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class SumOfFirstNNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+
+        int i=1;
+        while (i<=n){
+            sum +=i;
+            i++;
+        }
+        System.out.println("sum is :"+sum);
+    }
+}

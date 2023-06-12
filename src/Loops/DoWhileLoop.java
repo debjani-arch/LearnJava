b6fdbcf7372154a0f0184824454e8b7ec390cc33
debjani-arch <debjanide2017@gmail.com>
2023-06-12 13:39:58 +0530

    Do While Loop

---
+++ src/Loops/DoWhileLoop.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class DoWhileLoop {
+    private static int counter;
+
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("Hi There");
+            counter++;
+
+        }
+        while(counter <=0);
+    }
+}

641171f1a7b7fd7959c62d7fc6ad56ee64fd3028
debjani-arch <debjanide2017@gmail.com>
2023-06-12 11:15:30 +0530

    Print Reverse of a Number

---
+++ src/Loops/PrintReverseOfaNumber.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class PrintReverseOfaNumber {
+    public static void main(String[] args) {
+        int n = 109981;
+        while ( n>0){
+            int lastDigit = n%10;
+            System.out.print(lastDigit);
+            n = n/10;
+        }
+    }
+}

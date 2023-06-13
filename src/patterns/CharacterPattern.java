06b832090c8d2cc7d42ae52178159de190b82e48
debjani-arch <debjanide2017@gmail.com>
2023-06-13 14:25:59 +0530

    Character Pattern using for Loop

---
+++ src/patterns/CharacterPattern.java
@@ -0,0 +1,18 @@
+package patterns;
+
+public class CharacterPattern {
+    public static void main(String[] args) {
+        int n = 4;
+        char ch = 'A';
+        //outer Loop
+        for (int line = 1; line<=n; line++){
+            //for inner Loop
+            for (int chars=1; chars<=line; chars++){
+                System.out.print(ch);
+                ch++;
+
+            }
+            System.out.println();
+        }
+    }
+}

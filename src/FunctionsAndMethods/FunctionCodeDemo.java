f4c6170475c5f6d6c95fa08a42266f3e283a170d
debjani-arch <debjanide2017@gmail.com>
2023-06-14 16:06:37 +0530

    Function Code Demo

---
+++ src/FunctionsAndMethods/FunctionCodeDemo.java
@@ -0,0 +1,11 @@
+package FunctionsAndMethods;
+
+public class FunctionCodeDemo {
+    public static void printHelloWorld(){
+        System.out.println("Hello World");
+
+    }
+    public static void main(String[] args) {
+        printHelloWorld();
+    }
+}

b871d1b97cd14d69140b4e1624ede7f64da6e2f3
debjani-arch <debjanide2017@gmail.com>
2023-06-16 11:34:09 +0530

    Factorial Of a Number, n

---
+++ src/FunctionsAndMethods/FindFactorial.java
@@ -0,0 +1,19 @@
+package FunctionsAndMethods;
+
+public class FindFactorial {
+    public static int factorial(int n){
+        int f=1;
+
+        for (int i = 1; i<=n; i++){
+            f = f * i;
+        }
+        return f; //factorial of n
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(factorial (4));
+
+    }
+}
+
